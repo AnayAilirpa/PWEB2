7e1557c7ebe589ef46aef1271635948fea53323a
AnayAilirpa <165096298+AnayAilirpa@users.noreply.github.com>
2024-09-12 08:42:18 +0700

    Pweb2

--- Jobsheet1/2. Encapsulation.php
+++ Jobsheet1/2. Encapsulation.php
@@ -5,12 +5,10 @@ class Mahasiswa {
     private $nama;
     private $nim;
     private $jurusan;
-
-    //inisilasi
-    public function __construct($nama, $nim, $jurusan) {
-        $this->nama = $nama;
-        $this->nim = $nim;
-        $this->jurusan = $jurusan;
+    
+    //membuat metode tampilData untuk menampilkan data mahasiswa
+    public function tampilData() {
+        return "Nama: " . $this->nama . "<br> NIM: " . $this->nim . "<br> Jurusan: " . $this->jurusan . "<br>";
     }
 
     //metode getter
@@ -26,14 +24,20 @@ class Mahasiswa {
     }
 }
 
-//instansiasi
 echo "<p><b>2. Encapsulation</b></p>";
-$mahasiswa1 = new Mahasiswa("Yana Aprilia", "230202047", "Komputer dan Bisnis");
-//output metode getter
-echo $mahasiswa1->getMahasiswa()."<br>"; 
 
+//instansiasi
+$mahasiswa1 = new Mahasiswa();
+
+//set mahasiswa
+$mahasiswa1->setMahasiswa("Yana Aprilia", "230202047", "Komputer dan Bisnis");
+
+//get mahasiswa
+echo $mahasiswa1->getMahasiswa();  
+echo "<br>";
+
+//set mahasiswa baru
 $mahasiswa1->setMahasiswa("AnayAilirpa", "230202047", "Komputer dan Bisnis");
-//output metode setter
+//get Mahasiswa baru
 echo $mahasiswa1->getMahasiswa();  
 ?>
-
