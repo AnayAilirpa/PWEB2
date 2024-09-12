c5c5c0ba537e0016847cb752d3460a02fcebd4ee
AnayAilirpa <165096298+AnayAilirpa@users.noreply.github.com>
2024-09-12 08:32:21 +0700

    Pweb2

--- Jobsheet1/1. Membuat Class dan Object.php
+++ Jobsheet1/1. Membuat Class dan Object.php
@@ -5,23 +5,21 @@ class Mahasiswa {
     public $nama;
     public $nim;
     public $jurusan;
-
-    //inisilasi
-    public function __construct($nama, $nim, $jurusan) {
-        $this->nama = $nama;
-        $this->nim = $nim;
-        $this->jurusan = $jurusan;
-    }
-
+    
     //membuat metode tampilData untuk menampilkan data mahasiswa
     public function tampilData() {
-        return "Nama: ". $this->nama. "<br> NIM: ". $this->nim. "<br> Jurusan: ". $this->jurusan. "<br>";
+        return "Nama: " . $this->nama . "<br> NIM: " . $this->nim . "<br> Jurusan: " . $this->jurusan . "<br>";
     }
 }
 
-//instansiasi
 echo "<p><b>1. Membuat Class dan Object</b></p>";
-$mahasiswa1 = new Mahasiswa("Yana Aprilia", "230202047", "Komputer dan Bisnis");
+
+//instansiasi
+$mahasiswa1 = new Mahasiswa();
+$mahasiswa1->nama = "Yana Aprilia";
+$mahasiswa1->nim = "230202047";
+$mahasiswa1->jurusan = "Komputer dan Bisnis";
+
 //menampilkan data mahasiswa1
-echo $mahasiswa1->tampilData(); 
+echo $mahasiswa1->tampilData();
 ?>
